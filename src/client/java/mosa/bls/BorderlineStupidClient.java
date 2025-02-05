214e3e1669198e5b62b7ffa97e3b3c2afe25f27a
PokePuff05 <nataliehancock38@gmail.com>
2025-02-05 12:22:18 -0500

    first commit

---
+++ src/client/java/mosa/bls/BorderlineStupidClient.java
@@ -0,0 +1,10 @@
+package mosa.bls;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BorderlineStupidClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
